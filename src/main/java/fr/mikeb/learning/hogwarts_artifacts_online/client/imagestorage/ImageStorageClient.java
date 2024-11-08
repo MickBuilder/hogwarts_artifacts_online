b0d8b0a7d335e75411a86189814c5d548f3f20be
Mick <bmikdaam@gmail.com>
2024-11-08 18:07:21 +0100

    Implement upload image to Azure Blob Storage

---
+++ src/main/java/fr/mikeb/learning/hogwarts_artifacts_online/client/imagestorage/ImageStorageClient.java
@@ -0,0 +1,8 @@
+package fr.mikeb.learning.hogwarts_artifacts_online.client.imagestorage;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public interface ImageStorageClient {
+  String uploadImage(String containerName, String originalImageName, InputStream data, long length) throws IOException;
+}
